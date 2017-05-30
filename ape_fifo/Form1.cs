dc91d22fa852bae73d4d9c9409950293370c4f08
aleperalta <aperalta0@ucol.mx>
2017-05-29 21:33:51 -0500

    FIFO

---
+++ ape_fifo/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ape_fifo
+{
+    public partial class Form1frmPrincipal : Form
+    {
+        static Random rand = new Random();
+        Procesador proc = new Procesador();
+
+        public Form1frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimular_Click(object sender, EventArgs e)
+        {
+            int contadorDeVacio = 0;
+
+            for (int i = 0; i < 200; i++)
+            {
+                if (rand.Next(1,101) <= 25)
+                {
+                    Proceso nuevoP = new Proceso();
+                    proc.enqueue(nuevoP); //Meter proceso
+                }
+
+                Proceso vProceso = proc.peek(); //Ver primero de la cola
+                if (vProceso != null)
+                {
+                    vProceso.ciclos--;
+                    if (vProceso.ciclos == 0)
+                        proc.dequeue();
+
+                } else
+                    contadorDeVacio++;   //Si el procesador está vacío, suma 1 al contador
+            }
+
+            txtInformacion.Text = "Ciclos que estuvo vacía: " + contadorDeVacio.ToString() + Environment.NewLine
+                                    + proc.procesosPendientes() + Environment.NewLine;
+            
+        }
+    }
+}
---
+++ ape_fifo/Procesador.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ape_fifo
+{
+    class Procesador
+    {
+        Proceso primero, ultimo, temp;
+
+        public Procesador()
+        {
+            primero = null;
+            ultimo = null;
+        }
+
+        public void enqueue(Proceso nuevoP)   //Meter proceso
+        {
+            if (primero == null)
+            {
+                primero = nuevoP;
+                ultimo = nuevoP;
+            }
+            else
+            {
+                ultimo.siguiente = nuevoP;
+                ultimo = nuevoP;
+            }
+        }
+
+        public void dequeue()   //Sacar proceso
+        {
+            if (primero == null || ultimo == primero)
+            {
+                primero = null;
+                ultimo = null;
+            }
+            else
+                primero = primero.siguiente;
+        }
+
+        public Proceso peek()   //Ver proceso actual
+        {
+            return primero;
+        }
+
+        public string procesosPendientes()
+        {
+            int procPendientes = 0;
+            int sumaCiclosPendientes = 0;
+            temp = primero;
+
+            while (temp != null)
+            {
+                sumaCiclosPendientes += temp.ciclos;
+                procPendientes++;
+                temp = temp.siguiente;
+            }
+
+            string pendientes = "Procesos pendientes: " + procPendientes.ToString() + Environment.NewLine +
+                                "Suma de los ciclos pendientes: " + sumaCiclosPendientes.ToString();
+
+            return pendientes;
+        }
+    }
+}
